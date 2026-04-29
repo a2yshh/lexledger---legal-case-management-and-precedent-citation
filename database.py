1822ed48c59301f23cefddf40cd78212ebc43533
a2yshh <as9710@srmist.edu.in>
2026-04-29 18:33:14 +0530

    move db credentials to .env

--- database.py
+++ database.py
@@ -1,14 +1,17 @@
 import mysql.connector
 from mysql.connector import Error
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_db_connection():
     """Create database connection"""
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            database='legal_case_management',
-            user='ayush', 
-            password='ayush08'  
+            host=os.getenv('DB_HOST'),
+            database=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD')
         )
         if connection.is_connected():
             return connection
